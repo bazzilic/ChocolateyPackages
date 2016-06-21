ee927cea3134c75c9a58acde54a28a644a9e2040
bazzilic <bazzilic@gmail.com>
2016-06-21 21:07:11 +0800

    Renamed 'output' dir to '_output' ProductId renamed to ProductCode as it should be Proper handling of paths if the script is invoked from somewhere else Proper handling if some files or dirs do not exist (e.g. right after git checkout) Minor formatting tweaks All output is now done in UTF8 without BOM Added Far Manager icon to the repo

--- Far/generate.ps1
+++ Far/generate.ps1
@@ -1,8 +1,18 @@
-﻿$lastbuild   = (Get-Content 'lastbuild.txt' -First 1).Trim()
+$absPath = Split-Path -parent $PSCommandPath
+$toolsPath = "$absPath\tools\"
+$outputPath = "$(Split-Path -parent $absPath)\_output\"
+
+if ( -not ( Test-Path "$absPath\lastbuild.txt" ) )
+{
+    New-Item "$absPath\lastbuild.txt" -Type file | Out-Null
+    [IO.File]::WriteAllLines( "$absPath\lastbuild.txt", "1`n" )
+}
+
+$lastbuild   = (Get-Content "$absPath\lastbuild.txt" -First 1).Trim()
 $downloadUri = "http://farmanager.com/files/"
 
-$productIdx86 = ''
-$productIdx64 = ''
+$productCodex86 = ''
+$productCodex64 = ''
 
 $buildx86 = ''
 $buildx64 = ''
@@ -11,14 +21,14 @@ $x86filename = ''
 $x64filename = ''
 
 Write-Host
-Write-Host "`t== Far Manager choco package update script =="
-Write-Host "Last known build is" $lastbuild
+Write-Host "`t[ Far Manager choco package update script ]"
 Write-Host
+Write-Host "Last known build is" $lastbuild
 
-$webpage = (Invoke-WebRequest -URI "http://farmanager.com/download.php?l=en").content
+$webpage = ( Invoke-WebRequest -URI "http://farmanager.com/download.php?l=en" ).content
 
 # X86
-if ($webpage -match 'href="files/(Far30b\d+\.x86\.\d{8}\.msi)"')
+if ( $webpage -match 'href="files/(Far30b\d+\.x86\.\d{8}\.msi)"' )
 {
     $x86filename = $Matches[1]
     $x86filename -match 'Far30b(\d+)\.x86\.\d{8}\.msi' | Out-Null
@@ -30,7 +40,7 @@ if ($webpage -match 'href="files/(Far30b\d+\.x86\.\d{8}\.msi)"')
     {
         $tempfilename = [System.IO.Path]::GetTempFileName()
         Invoke-WebRequest -URI $downloadUri$x86filename -OutFile $tempfilename
-        $productIdx86 = & ".\msi-get.exe" $tempfilename
+        $productCodex86 = & "$absPath\msi-get.exe" $tempfilename
     }
     else
     {
@@ -41,7 +51,7 @@ if ($webpage -match 'href="files/(Far30b\d+\.x86\.\d{8}\.msi)"')
 }
 
 #X64
-if ($webpage -match 'href="files/(Far30b\d+\.x64\.\d{8}\.msi)"')
+if ( $webpage -match 'href="files/(Far30b\d+\.x64\.\d{8}\.msi)"' )
 {
     $x64filename = $Matches[1]
     $x64filename -match 'Far30b(\d+)\.x64\.\d{8}\.msi' | Out-Null
@@ -49,7 +59,7 @@ if ($webpage -match 'href="files/(Far30b\d+\.x64\.\d{8}\.msi)"')
 
     Write-Host "Found a link to a stable x64 build" $buildx64
 
-    if ( $buildx64 -ne $buildx86)
+    if ( $buildx64 -ne $buildx86 )
     {
         Write-Host "x86 build" $buildx86 "and x64 build" $buildx64 "mismatch."
         Write-Host "Try again later"
@@ -61,7 +71,7 @@ if ($webpage -match 'href="files/(Far30b\d+\.x64\.\d{8}\.msi)"')
     {
         $tempfilename = [System.IO.Path]::GetTempFileName()
         Invoke-WebRequest -URI $downloadUri$x64filename -OutFile $tempfilename
-        $productIdx64 = & ".\msi-get.exe" $tempfilename
+        $productCodex64 = & "$absPath\msi-get.exe" $tempfilename
     }
     else
     {
@@ -71,22 +81,41 @@ if ($webpage -match 'href="files/(Far30b\d+\.x64\.\d{8}\.msi)"')
     }
 }
 
-echo $buildx64 | Out-File 'lastbuild.txt' -Encoding utf8
+[IO.File]::WriteAllLines( "$absPath\lastbuild.txt", $buildx64 ) # currently the only way to write UTF8 without BOM
 
 Write-Host
 
-Write-Host "ProductId for x86 MSI:" $productIdx86
-Write-Host "ProductId for x64 MSI:" $productIdx64
+Write-Host "ProductCode for x86 MSI:" $productCodex86
+Write-Host "ProductCode for x64 MSI:" $productCodex64
 
 Write-Host
 
-cat 'chocolateyUninstall.ps1.template' | % { $_ -replace   "<<<ProductIDx86>>>" ,  $productIdx86 }             | % { $_ -replace   "<<<ProductIDx64>>>" ,  $productIdx64 }             | Out-File 'tools\chocolateyUninstall.ps1' -Encoding utf8
-cat 'chocolateyInstall.ps1.template'   | % { $_ -replace "<<<X86DownloadLink>>>", "$downloadUri$x86filename" } | % { $_ -replace "<<<X64DownloadLink>>>", "$downloadUri$x64filename" } | Out-File 'tools\chocolateyInstall.ps1'   -Encoding utf8
-cat 'Far.nuspec.template' | % { $_ -replace "<<<Build>>>", $buildx86 } | Out-File 'Far.nuspec' -Encoding utf8
+if ( -not ( Test-Path $toolsPath ) )
+{
+    md $toolsPath | Out-Null
+}
 
-& "chocolatey" @("pack", "Far.nuspec")
+$content = ''
 
-mv "Far.3.0.$buildx64.nupkg" "..\output\" -Force
+$content = cat "$absPath\chocolateyUninstall.ps1.template" | % { $_ -replace "<<<ProductCodex86>>>", $productCodex86 } | % { $_ -replace "<<<ProductCodex64>>>", $productCodex64 }
+[IO.File]::WriteAllLines( "$absPath\tools\chocolateyUninstall.ps1", $content )
 
-Write-Host "Package could be found in '..\output\' folder"
+$content = cat "$absPath\chocolateyInstall.ps1.template" | % { $_ -replace "<<<X86DownloadLink>>>", "$downloadUri$x86filename" } | % { $_ -replace "<<<X64DownloadLink>>>", "$downloadUri$x64filename" }
+[IO.File]::WriteAllLines( "$absPath\tools\chocolateyInstall.ps1", $content )
+
+$content = cat "$absPath\Far.nuspec.template" | % { $_ -replace "<<<Build>>>", $buildx86 }
+[IO.File]::WriteAllLines( "$absPath\Far.nuspec", $content )
+
+Push-Location -Path $absPath
+& "choco" @("pack", "$absPath\Far.nuspec")
+Pop-Location
+
+if ( -not ( Test-Path $outputPath ) )
+{
+    md $outputPath | Out-Null
+}
+
+mv "$absPath\Far.3.0.$buildx64.nupkg" $outputPath -Force
+
+Write-Host "Package could be found in $outputPath"
 Write-Host
